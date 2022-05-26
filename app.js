0f5714d8d4fa60b01b7b1df6878293b726830e17
Michael Crawford <webdevcrawford@gmail.com>
2022-05-25 20:42:46 -0600

    adds tasks, features, and level up instructions

--- app.js
+++ app.js
@@ -16,3 +16,46 @@ function getGuess() {
 }
 
 console.log(getGuess())
+
+
+// ## Features
+
+// - Allow the player to continually be prompted to enter their guess at the secret number until they guess correctly.
+// - If the player has an incorrect guess, display an alert message that informs the player:
+//     - Whether their guess is too high or too low, and…
+//     - A list of all the previously guessed numbers (without showing the square brackets of an array).
+// - If the player has guessed the secret number:
+//     - Display an alert message that congrats the player and informs them of how many guesses they took.
+//     - End the gameplay.
+
+// ## Tasks
+
+// Completing the following tasks will result in a working *Guess the Number* game:
+
+// 1. Add a `prevGuesses` property to the `game` object initialized to an empty array.
+// 2. Add a `getGuess` method to `game` that prompts the player to enter a guess with a message formatted as: *Enter a guess between [smallestNum] and [biggestNum].*
+// Hint - use a template literal for the prompt message.
+// 3. Ensure that the `getGuess` method returns a value that:
+//     - Is a *number*, not a *string*.
+//     - Is between `smallestNum` and `biggestNum`, inclusive.
+//     - Hints:
+//         - This is a great use case for a `while` loop.
+//         - `parseInt` returns `NaN` if the string cannot be parsed into a number.
+// 4. From within the `play` method, invoke the `getGuess` method from inside a loop, and add the new guess to the `prevGuesses` array.
+//     - Hint: this is an excellent use for a while loop (or even a do...while loop!)
+// 5. Add a `render` method to `game` that `play` will call after a guess has been made that alerts:
+//     - If the secret has been guessed: `Congrats! You guessed the number in [number of prevGuesses]!`
+//     - Otherwise: `Your guess is too [high|low] Previous guesses: x, x, x, x`
+//     - Hints:
+//         - `render` won’t be able to access any of `play`’s local variables, e.g., `guess`, so be sure pass `render` any arguments as needed (you may not have built your program to use any, that's ok if that's the case!)
+//         - Template literals not only have interpolation, but they also honor whitespace - including line breaks!
+//         - The list of previous guesses can be generated using the array `join` method.
+// 6. The `play` method should end (`return`) when the guess matches `secretNum`.
+
+// ## Level Up 🚀
+
+// - If the player enters a number greater than the `secretNum` make it the new `biggestNum`, so that the player can't enter a number greater than it. If the player enters a number that is less than the `secretNum` make it the new `smallestNum`, so that the player can't enter a number less than it. Hint: You may want to add a helper function to accomplish this.
+
+// ## More Level Up 🚀
+
+// When `play` is run, immediately prompt the player to enter the smallest and biggest numbers instead of pre-setting values.

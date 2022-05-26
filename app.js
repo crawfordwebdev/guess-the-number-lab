0719c56529c0464ba80ab89634b9677dcbbdd8e6
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-05-26 14:34:52 -0600

    adds broken code before removal

--- app.js
+++ app.js
@@ -10,51 +10,64 @@ const game = {
 
     do {
       this.currentGuess = this.getGuess()
-      this.currentGuess = this.checkValidNumberChoice()
-      console.log(this.currentGuess)
-      if (this.currentGuess !== false ||
+      console.log("Current Guess: " + this.currentGuess)
+      if (this.currentGuess !== NaN &&
         this.currentGuess !== null) {
-          this.currentGuess = parseInt(this.currentGuess)
+        if (this.winCheck() === true && this.prevGuesses.length > 0)
           this.prevGuesses.push(this.currentGuess)
-          if (this.currentGuess !== this.secretNum ) {
-            this.guessAgainMessage()
-          }
+        if (this.winCheck() === false && this.currentGuess !== NaN && this.currentGuess !== null) {
+          this.guessAgainMessage()
+        }
       }
+
+
       // https://stackoverflow.com/questions/12864582/javascript-prompt-cancel-button-to-terminate-the-function
       // using cancel to get out of this
-    } while (this.currentGuess !== this.secretNum && this.currentGuess !== null)
+      console.log("Win Check: " + this.winCheck())
+    } while (this.winCheck() || this.currentGuess !== null)
 
     if (this.currentGuess === this.secretNum) {
       this.secretNumGuessed()
     }
-    alert(`Refresh the page to play again!`)
   },
-  checkValidNumberChoice: function () {
-    // REFACTOR ternary
+  getGuess: function () {
+    this.currentGuess = prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.
+    Hint: The Secret Number is: ${this.secretNum}`)
+    console.log("Get Guess: " + this.currentGuess)
     if (parseInt(this.currentGuess) >= this.smallestNum && parseInt(this.currentGuess) <= this.biggestNum) {
+      return parseInt(this.currentGuess)
+    } else if (this.currentGuess === null) {
+      return null
+    } else if (parseInt(this.currentGuess) === NaN){
+      return NaN
+    }
+  },
+  winCheck: function () {
+    if (this.currentGuess === this.secretNum) {
       return true
-    } else  {
+    } else {
       return false
     }
   },
   guessAgainMessage: function () {
-    let theGuess = this.currentGuess
     let messageHighOrLow = function () {
       // REFACTOR ternary
-      if (theGuess > this.secretNum) {
+      if (this.currentGuess > this.secretNum) {
         return "high"
-      } else {
+      } else if (this.currentGuess < secretNum) {
         return "low"
       }
     }
     return alert(`Your guess of ${this.currentGuess} is too ${messageHighOrLow()}.
     Previous Guesses: ${this.prevGuesses.join(', ')}`)
   },
-  getGuess: function () {
-    return prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`)
-  },
+
   secretNumGuessed: function () {
-    return alert(`Congratulations! You guessed the secret number in ${this.prevGuesses.length} tries!`)
+    if (this.prevGuesses.length > 1) {
+      return alert(`Congratulations! You guessed the secret number in ${this.prevGuesses.length} tries!`)
+    } else {
+      return alert(`Congratulations! You guessed the secret number in one try!`)
+    }
   },
   prevGuesses: [],
 }

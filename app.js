ac35b0644c6df8377ba767307141b120423724d7
Michael Crawford <webdevcrawford@gmail.com>
2022-05-26 21:41:50 -0600

    adds debugging console commands

--- app.js
+++ app.js
@@ -2,20 +2,30 @@ const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
   smallestNum: 1,
-  secretNum: 1,
+  secretNum: null,
   play: function () {
     this.secretNum = Math.floor(Math.random() *
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
     
-    this.prevGuesses.push(getGuess())
+    do {
+      // getGuess should only return a valid guess, otherwise it will keep asking.
+      console.log("Start Do: " + this.prevGuesses)
+      this.prevGuesses.push(this.getGuess())
+      console.log("After Push: " + this.prevGuesses)
+
+    // Did the play guess the correct answer?
+    } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum)
 
   },
   getGuess: function () {
+    // Need to declare the player's guess up here so we don't get stuck in the while loop
     let guess
 
     do {
-      guess = parseInt(prompt(``))
-
+      console.log('Getting Guess')
+      guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`))
+      console.log("Type of guess: " + typeof(guess))
+      console.log("What is the guess? : " + guess)
     } while (
       // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN
       // Must use isNaN because NaN === NaN will return false
